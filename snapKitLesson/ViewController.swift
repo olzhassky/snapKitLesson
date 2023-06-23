c9b36558c8aedde3e74bb2e3f9f5dee1e35fbbdd
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-06-23 19:42:24 +0600

    Initial Commit

---
+++ snapKitLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapKitLesson
+//
+//  Created by Olzhas Zhakan on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
